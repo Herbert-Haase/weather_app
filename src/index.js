e4cd46dc0208cfb930e5ccac74c4797ffdd25399
Herbert-Haase <herberthaase02@gmx.de>
2024-03-28 18:16:55 +0100

    access weather data via API

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 import "./styles/style.css";
 import "./styles/reset.css";
+
+async function getWeather(location) {
+  const response = await fetch(
+    `https://api.weatherapi.com/v1/current.json?key=31b9ec0ff0dc44d391d195204242203&q=${location}`
+  );
+  return await response.json();
+}
+// const weather = getWeather("london");
+// weather.then(console.log);
+console.log(getWeather("london"));
+
+async function getReleventInfo(info) {
+  const weatherInfo = await getWeather("berlin");
+}
